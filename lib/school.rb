340d458f9871bd6fb78fcb8b85c536191a625e49
Erin Stalzer <erinstalzer@gmail.com>
2019-04-23 19:05:06 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+
+  attr_accessor :grade
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+
+
+
+  def add_student(name, grade)
+    if @roster[:grade].empty?
+      @roster[:grade] = []
+    end
+  @roster[:grade] << name
+  end
+
+  def grade(grade)
+    @roster[:grade]
+  end
+
+
+
+end
